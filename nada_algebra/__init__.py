c407b388e4592391ff6422b9d60f5683c1e21913
Mathias Leys <mathias.leys@nillion.com>
2024-06-04 23:40:47 +0200

    Remove duplicate imports

--- nada_algebra/__init__.py
+++ nada_algebra/__init__.py
@@ -1,7 +1,6 @@
 """This is the __init__.py module"""
 
 from nada_algebra.array import NadaArray
-from nada_algebra.types import Rational, SecretRational
 from nada_algebra.funcs import *
 from nada_algebra.types import (
     Rational,
