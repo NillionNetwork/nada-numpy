a510cdb6897fbe9f6ae4f8263369d2a84e4ecb9e
Mathias Leys <mathias.leys@nillion.com>
2024-06-13 23:11:12 +0100

    Test fix

--- tests/nada-tests/src/logistic_regression.py
+++ tests/nada-tests/src/logistic_regression.py
@@ -6,10 +6,10 @@ import nada_algebra as na
 def nada_main():
     parties = na.parties(3)
 
-    weights = na.array((3,), parties[0], "A", na.SecretInteger)
+    weights = na.array((3,), parties[0], "A", SecretInteger)
     # This example does not support an array since we work with secret integers
     bias = SecretInteger(Input("bias", parties[0]))
-    x = na.array((3,), parties[1], "B", na.SecretInteger)
+    x = na.array((3,), parties[1], "B", SecretInteger)
 
     res = weights.dot(x)
     res += bias
