acdc59b8ed88b1718813335218407dbe1b596f54
José Cabrero-Holgueras <jose.cabrero@nillion.com>
2024-08-23 07:59:42 +0000

    feat: Improved tests for broadcasting

---
+++ tests/nada-tests/src/array_comparison.py
@@ -0,0 +1,33 @@
+from types import NoneType
+
+import numpy as np
+from nada_dsl import *
+
+import nada_numpy as na
+from nada_numpy.nada_typing import NadaInteger, NadaRational
+
+
+def nada_main():
+    parties = na.parties(2)
+
+    a = na.array([3], parties[0], "A", SecretInteger)
+    b = na.NadaArray(np.array([]))
+    c = na.NadaArray(np.array([na.rational(1.5)]))
+
+    assert not a.empty
+    assert b.empty
+    assert not c.empty
+
+    assert a.dtype == NadaInteger, a.dtype
+    assert b.dtype == None, b.dtype
+    assert c.dtype == NadaRational, c.dtype
+
+    assert a.ndim == 1, a.ndim
+    assert b.ndim == 1, b.ndim
+    assert c.ndim == 1, c.ndim
+
+    assert len(a) == 3, len(a)
+    assert len(b) == 0, len(b)
+    assert len(c) == 1, len(c)
+
+    return a.output(parties[1], "my_output")
